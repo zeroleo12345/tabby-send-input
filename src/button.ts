b2ac348aec186bc00cfc77d9f119a1c329d6a6c9
zeroleo12345 <zeroleo12345@163.com>
2026-02-09 11:52:57 +0800

    fix: this.config.changed$.subscribe after config ready

--- src/button.ts
+++ src/button.ts
@@ -16,9 +16,10 @@ export class QuickCmdButtonProvider extends ToolbarButtonProvider {
 
         this.config.ready$.toPromise().then(() => {
             this.reload_hotkey()
-        })
-        this.config.changed$.subscribe(() => {
-            this.reload_hotkey()
+
+            this.config.changed$.subscribe(() => {
+                this.reload_hotkey()
+            })
         })
         // Listen for hotkey matches
         this.hotkeys.hotkey$.subscribe(async (hotkey_id) => {
