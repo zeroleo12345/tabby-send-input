fd419ccfd61cb0e6ede9a570300a81d2a426f606
zeroleo12345 <zeroleo12345@163.com>
2026-03-10 14:35:33 +0800

    fix

--- src/components/quickCmdsSettingsTab.component.ts
+++ src/components/quickCmdsSettingsTab.component.ts
@@ -65,10 +65,10 @@ export class QuickCmdsSettingsTabComponent {
         modal.componentInstance.command = { ...command, group: command.group || 'Ungrouped' }
         modal.componentInstance.allGroups = Array.from(new Set(this.commands.map(x => x.group || ''))).filter(x => x)
         modal.result.then(result => {
+            // If the group is 'Ungrouped', set it to null
             if (result.group === 'Ungrouped') {
                 result.group = null
             }
-            // If the group is 'Ungrouped', set it to null
             /*
             // 从UI修改 QuickCmds: name, shortcut, text, group
             export interface QuickCmds {
